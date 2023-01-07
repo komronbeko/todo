7ee1a52b66e4c24ede93f5c7e4a79c424c79e33b
kolimov0825@gmail.com <kolimov0825@gmail.com>
2023-01-07 16:29:17 +0500

    filtered

--- index.js
+++ index.js
@@ -3,75 +3,93 @@ let modeInput = document.querySelector(".mode-input");
 let form = document.querySelector(".form");
 let todoInput = document.querySelector(".todo-input");
 let todoList = document.querySelector(".todo-list");
+let filter = document.querySelector("#filter");
 
-modeInput.addEventListener("click", function(){
-   body.classList = [
-    modeInput.checked ? "dark-mode" : "light-mode"
-   ]
-})
+modeInput.addEventListener("click", function () {
+  body.classList = [modeInput.checked ? "dark-mode" : "light-mode"];
+});
 
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-form.addEventListener("submit", function(e) {
-   e.preventDefault();
+  //// create li ///
+  let item = document.createElement("li");
+  item.classList.add("item");
 
+  //// create div ///
+  let div = document.createElement("div");
+  div.classList.add("item__div");
 
-   //// create li ///
-   let item = document.createElement("li");
-   item.classList.add("item");
+  //// create btn to check ///
+  let check = document.createElement("btn");
+  check.classList.add("check-btn");
+  // check.type = "checkbox";
+  div.append(check);
 
-   //// create div ///
-   let div = document.createElement("div");
-   div.classList.add("item__div");
+  //// take input value ////
+  let text = document.createElement("p");
+  text.classList.add("text");
+  text.innerHTML = todoInput.value;
+  div.append(text);
 
-   //// create btn to check ///
-   let check = document.createElement("btn")
-   check.classList.add("check-btn");
-   // check.type = "checkbox";
-   div.append(check);
+  let remove = document.createElement("p");
+  remove.classList.add("remove");
+  div.append(remove);
 
-   //// take input value ////
-   let text = document.createElement("p")
-   text.classList.add("text");
-   text.innerHTML = todoInput.value;
-   div.append(text);
+  todoInput.value = "";
+//   item.append(div);
+  todoList.append(div);
+});
 
-   let remove = document.createElement("p")
-   remove.classList.add("remove");
-   div.append(remove);
-   
+todoList.addEventListener("click", function (e) {
+  e.preventDefault();
+  let item = e.target;
 
+  if (item.classList.contains("check-btn")) {
+    let todo = item.parentElement;
 
+    if (!todo.classList.contains("completed")) {
+      todo.classList.add("completed");
+    } else {
+      todo.classList.remove("completed");
+    }
+  }
 
-   todoInput.value = '';
-   item.append(div);
-   todoList.append(item);
-})
-
-
-todoList.addEventListener("click", function(e){
-   e.preventDefault();
-   let item = e.target;
-
-   if(item.classList.contains("check-btn")){
-      let todo = item.parentElement;
-
-      if(!todo.classList.contains("completed")){
-         todo.classList.add("completed");
-      } else{
-         todo.classList.remove("completed");
-      }
-   }
-
-   if(item.classList.contains("remove")){
-      let todo = item.parentElement;
-      todo.classList.add("deleted");
-      todo.addEventListener("clicked", e => {
-         todo.remove();
-      })
-   }
-})
+  if (item.classList.contains("remove")) {
+    let todo = item.parentElement;
+    todo.classList.add("deleted");
+    todo.addEventListener("clicked", (e) => {
+      todo.remove();
+    });
+  }
+});
 
 
 
+filter.addEventListener("click", (e) => {
+   // console.log(e.target.value)
+   let option = e.target.value
+  let todoInputs = todoList.childNodes;
+  todoInputs.forEach((todo) => {
+    switch (option) {
+      case "all":
+        todo.style.display = "flex";
+        break;
 
-
+      case "active":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break; 
+    }
+  });
+});
